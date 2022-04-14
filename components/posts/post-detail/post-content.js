751c5f4a38a26475bec6880aff0d2f475a2c3b3b
davitlabadze <labadzedato18@gmail.com>
2022-04-15 00:28:34 +0400

    refactor(util): single post function

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,20 +1,13 @@
 import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header'
 
-const DUMMY_POSTS = {
-    slug: 'next',
-    title: 'Getting started with nextJS',
-    image: 'next.png',
-    date: '2022-05-14',
-    content: '# This is a first post',
-};
 
-
-function PostContent() {
-    const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`
+function PostContent(props) {
+    const {post} = props;
+    const imagePath = `/images/posts/${post.slug}/${post.image}`
     return <article className=' w-fit mt-12 bg-gray-500 shadow-xl rounded-xl p-6'>
-        <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
-        <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
+        <PostHeader title={post.title} image={imagePath} />
+        <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
 }
 export default PostContent;
